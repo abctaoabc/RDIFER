4fa16bfb7d0d3290a83a3c15defa99c72f910420
le <1290696647@qq.com>
2023-12-05 17:26:08 +0800

    调通MAE+Resnet

--- model.py
+++ model.py
@@ -14,18 +14,16 @@ class FERAE(nn.Module):
             patch_size=16, embed_dim=768, depth=12, num_heads=12,
             decoder_embed_dim=512, decoder_depth=8, decoder_num_heads=16,
             mlp_ratio=4, norm_layer=partial(nn.LayerNorm, eps=1e-6))
-        # self.cnn = UNet()
-        # self.to_next_proj = nn.Linear(kwargs["embed_dim"], kwargs["patch_size"]**2 * kwargs["in_chans"])
+        # self.to_next_proj = nn.Linear(kwargs["embed_dim"], kwargs["patch_size"]**2 * kwargs["in_chans"]) TODO: add after mae_encoder, equally project
 
         self.decomposer = decomposer_net(args=None, NumOfLayer=18)
 
-        self.classify = nn.Linear(512,7)
+        self.classify = nn.Linear(512, 7)
 
     def forward(self, x):
-
         # MAE encoder
         x = self.mae_encoder(x)
-        x = x[:, 1:, :] #remove cls token
+        x = x[:, 1:, :]  # remove cls token
         x = self.mae_encoder.unpatchify(x)
         feat_domain, feat_exp = self.decomposer(x)
         logit_exp = self.classify(feat_exp)
